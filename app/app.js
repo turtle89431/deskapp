44328ec86b8bc35d2207dc21f334c5a1b46e6da6
Walter Winser <turtle89431@gmail.com>
2019-06-10 12:22:00 -0700

    update

---
+++ app/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from './componets/main'
+
+var mountNode = document.getElementById("yn");
+ReactDOM.render(<Main name="Walter" />, mountNode);
