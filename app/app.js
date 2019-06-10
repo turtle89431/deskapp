f3b02ed15b9acb78558697ead4a1a73416f8323b
Walter Winser <turtle89431@gmail.com>
2019-06-10 16:52:34 -0700

    added dist option

--- app/app.js
+++ app/app.js
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom";
 import Main from './componets/main'
 
 var mountNode = document.getElementById("yn");
-ReactDOM.render(<Main name="Walter" />, mountNode);
+var p = "your name here ?"
+ReactDOM.render(<Main name={p} />, mountNode);
--- app/componets/main.js
+++ app/componets/main.js
@@ -1,6 +1,6 @@
 import React from "react";
 export default class Main extends React.Component {
     render() {
-      return <div>Hello 3 {this.props.name}</div>;
+      return <div>example {this.props.name}</div>;
     }
   }
